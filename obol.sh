27ea08973dda1e6ee71a64fb45c9ed0a8b9a9fa3
zycode08 <zycode08@gmail.com>
2023-02-27 17:36:43 -0600

    add obol script

---
+++ obol.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget -O get-docker.sh https://get.docker.com
+sudo bash get-docker.sh
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
+cd charon-distributed-validator-node
+mkdir .charon
+sudo chmod -R 777 .charon
+
+docker run --rm -v "$(pwd):/opt/charon" obolnetwork/charon:v0.13.0 create enr | tee $HOME/keys.txt
+private_key=$(sudo cat .charon/charon-enr-private-key)
+sed -i -e "s/^Created ENR private key:.*/Created ENR private key: $private_key/g" $HOME/keys.txt
